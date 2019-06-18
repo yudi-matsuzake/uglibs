6333f4e60af90efda28dc3d816dad7b5f3d524e6
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2019-06-18 01:06:15 -0300

    Implement zoom in and zoom out

--- libs/graphics/app.cpp
+++ libs/graphics/app.cpp
@@ -15,6 +15,24 @@ void operator*=(rect2d& r, float scalar)
 	r.width *= scalar;
 }
 
+void operator+=(rect2d& r, glm::vec2 const& v)
+{
+	r.position += v;
+}
+
+void operator-=(rect2d& r, glm::vec2 const& v)
+{
+	r.position -= v;
+}
+
+glm::vec2 rect2d::center() const
+{
+	return {
+		position.x + width/2.f,
+		position.y + height/2.f
+	};
+}
+
 /*
  * callbacks
  * =========
