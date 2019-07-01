b1846267fb52c8dcc6f9858c03b34e14e36a7a6b
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2019-07-01 14:41:57 -0300

    Implement a grid base

---
+++ libs/graphics/grid2d-render.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <array>
+#include <string_view>
+
+#include "graphics/misc.hpp"
+#include "graphics/shader.hpp"
+#include "graphics/program.hpp"
+#include "graphics/vao.hpp"
+#include "graphics/buffers.hpp"
+
+namespace graphics{
+
+class grid2d_render {
+public:
+
+	grid2d_render();
+
+	/**
+	  * renders a grid in the rect `rect`
+	  */
+	void operator()(
+		glm::mat4 const& p,
+		glm::mat4 const& v,
+		rect2d const& rect,
+		color const& c
+	);
+
+protected:
+
+	vertex_shader m_vertex_shader;
+	fragment_shader m_fragment_shader;
+
+	program m_program;
+
+	vao m_vscr_vao;
+	vbo m_vscr_vbo;
+	ebo m_vscr_ebo;
+};
+
+}
