d4669d662df5f7c7a7f40acf475ea11b6d05549b
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2019-08-21 21:53:56 -0300

    Abstract some opengl functions for texture

--- libs/graphics/texture2d.hpp
+++ libs/graphics/texture2d.hpp
@@ -2,17 +2,51 @@
 
 #include <cstdint>
 
+#include "graphics/misc.hpp"
+
 namespace graphics{
+
+enum class wrap_type : int32_t {
+	clamp_to_edge		= GL_CLAMP_TO_EDGE,
+	clamp_to_border		= GL_CLAMP_TO_BORDER,
+	mirrored_repeat		= GL_MIRRORED_REPEAT,
+	repeat			= GL_REPEAT,
+	mirror_clamp_to_edge	= GL_MIRROR_CLAMP_TO_EDGE
+};
+
+enum class filter_type : int32_t {
+	NEAREST = GL_NEAREST,
+	LINEAR = GL_LINEAR
+};
 
 class texture2d{
 public:
 
-	texture2d(int32_t width, int32_t height, void* data);
+	texture2d();
+	texture2d(
+		uint8_t* data,
+		int32_t width,
+		int32_t height,
+		wrap_type s_wrap = wrap_type::repeat,
+		wrap_type t_wrap = wrap_type::repeat,
+		filter_type min_filter = filter_type::LINEAR,
+		filter_type mag_filter = filter_type::LINEAR
+	);
+
 	virtual ~texture2d();
 
 	uint32_t id() const;
+	void bind(uint32_t texture_unit = 0) const;
 
-	void bind() const;
+	void set_texture_wrap_s(wrap_type wrap);
+	void set_texture_wrap_t(wrap_type wrap);
+
+	void set_texture_magnifier_filter(filter_type filter);
+	void set_texture_minifier_filter(filter_type filter);
+
+	void generate_mipmap();
+
+	void set_data(uint8_t* data, int32_t width, int32_t height);
 
 protected:
 	uint32_t m_id;
