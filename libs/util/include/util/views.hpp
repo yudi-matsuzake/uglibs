d9ee6b943c1400a784aaa99fe54db40a6b476de7
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2022-01-28 16:15:39 -0300

    Implement simplexization tests based on area

--- libs/util/include/util/views.hpp
+++ libs/util/include/util/views.hpp
@@ -5,9 +5,14 @@
 
 #pragma once
 
+#include <concepts>
+
 #include "range/v3/view.hpp"
 
 namespace util{
+
+namespace rgs = std::ranges;
+namespace vws = std::views;
 
 namespace views = ranges::views;
 
@@ -22,5 +27,24 @@ auto numbers(T start, T finish, T step = T{1})
 		}
 	);
 }
+
+struct with_temporary_view{};
+static constexpr auto with_temporary = with_temporary_view{};
+
+template<rgs::input_range R>
+constexpr rgs::view auto operator|(R&& r, with_temporary_view)
+	requires (std::default_initializable<rgs::range_value_t<R>>
+		&& std::copy_constructible<rgs::range_value_t<R>>)
+{
+	using T = rgs::range_value_t<R>;
+	return r | vws::transform(
+		[tmp = T{}](T&& x) mutable -> T&
+		{
+			tmp = x;
+			return tmp;
+		}
+	);
+}
+
 
 } // end of namespace util
